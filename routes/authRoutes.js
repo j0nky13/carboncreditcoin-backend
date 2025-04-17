e7dbd13af1769a3ab201a65ee5c4694cfa5ad6d7
Jonathan <jonkyle@me.com>
2025-04-17 17:44:51 -0400

    Fix: import sendOTP and verifyOTP

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,5 @@
 import express from 'express';
-import { registerUser } from '../controllers/authController.js';
-
+import { registerUser, sendOTP, verifyOTP } from '../controllers/authController.js';
 const router = express.Router();
 
 router.post('/register', registerUser);
